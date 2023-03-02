53a4f5a769e3418de4eaf1ea9f9be6db6f7f2821
monisha3025 <monishamonisha0041@gmail.com>
2023-03-02 11:47:09 +0700

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,7 +19,7 @@ function About() {
           innovations, by making our products more sustainably, by building a
           creative and diverse global team and by making a positive impact in
           communities where we live and work. Based in Beaverton, Oregon, NIKE,
-          Inc. incluascacvfvz des the Nike, Converse, and Jordan brands.
+          Inc. includes the Nike, Converse, and Jordan brands.
         </p>
       </div>
     </div>
