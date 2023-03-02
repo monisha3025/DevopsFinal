ca72f1899938c1ad1fb1ab464c80256cf05794ea
monisha3025 <monishamonisha0041@gmail.com>
2023-03-02 11:42:34 +0700

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ function About() {
           Our missioddn is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreadddking sport
           innovations, by making our products more sustainably, by building a
-          creative and diverseacadcadc global team and by making a positive impact in
+          creative and diverse global team and by making a positive impact in
           communities where sacascawe live and work. Based in Beaverton, Oregon, NIKE,
           Inc. incluascacvfvz des the Nike, Converse, and Jordan brands.
         </p>
